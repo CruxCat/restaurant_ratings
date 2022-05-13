107d563b06dcf3836835f13f0ce11ba9b4a9028d
Catherine Vu <89547780+CruxCat@users.noreply.github.com>
2022-05-12 19:57:00 -0700

    initial commit

---
+++ ratings.py
@@ -0,0 +1,9 @@
+"""Restaurant rating lister."""
+
+# put your code here
+# open scores.txt and save into a variable
+# convert variable into string
+# store strings into dictionary
+# split string by colon
+# iterate over list - save first half as key and other half as value
+# sort(dict.keys())
