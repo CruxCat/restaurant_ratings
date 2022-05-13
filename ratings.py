be06225a16bca6eb7a33d21015954b11ba4536d9
Catherine Vu <89547780+CruxCat@users.noreply.github.com>
2022-05-12 20:46:56 -0700

    defined sorting function

--- ratings.py
+++ ratings.py
@@ -5,6 +5,13 @@
 scores = open("scores.txt")
 # create an empty dictionary
 ratings_dictionary = {}
+
+def sorting_dict(dict):
+    ratings_list = dict.items()
+    sorted_ratings_list = sorted(ratings_list)
+    for restaurant, rating in sorted_ratings_list:
+        print(f"{restaurant} has a rating of {rating}")
+
 # loop to iterate over scores.txt file
 for score in scores:
     score = score.rstrip()
@@ -12,24 +19,26 @@ for score in scores:
     # print(scores_list)
     ratings_dictionary[scores_list[0]] = int(scores_list[1])
 
-ratings_list = ratings_dictionary.items()
+sorting_dict(ratings_dictionary)
+# ratings_list = ratings_dictionary.items()
 
-sorted_ratings_list = sorted(ratings_list)
+# sorted_ratings_list = sorted(ratings_list)
 
-for restaurant, rating in sorted_ratings_list:
-    print(f"{restaurant} has a rating of {rating}")
+# for restaurant, rating in sorted_ratings_list:
+#     print(f"{restaurant} has a rating of {rating}")
 
 restaurant_input = input("What restaurant do you want to add? ")
 score_input = input("How do you score it? ")
 
 ratings_dictionary[restaurant_input] = score_input
 
-ratings_list = ratings_dictionary.items()
+sorting_dict(ratings_dictionary)
+# ratings_list = ratings_dictionary.items()
 
-sorted_ratings_list = sorted(ratings_list)
+# sorted_ratings_list = sorted(ratings_list)
 
-for restaurant, rating in sorted_ratings_list:
-    print(f"{restaurant} has a rating of {rating}")
+# for restaurant, rating in sorted_ratings_list:
+#     print(f"{restaurant} has a rating of {rating}")
 
 # convert variable into string
 # scores_str = scores.read()
